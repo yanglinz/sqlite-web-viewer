eaf8412c601afe5c8376f217f7aa49cded0b0acc
Yanglin Zhao <yz1876@gmail.com>
2022-04-19 21:02:58 -0400

    Add init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import ReactDOM from 'react-dom';
+
+function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+function init() {
+    ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(<h1>Hello, world!</h1>); 
+}
+
+ready(init);
