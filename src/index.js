7c8303e6613ce00eb1f8235ae103e6aa3ad26b1a
Yanglin Zhao <yz1876@gmail.com>
2022-04-20 21:21:54 -0400

    Remove useless comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import Root from "./components/Root";
 
-import { saveDB } from "./lib/sqlite";
+import { debugDB } from "./lib/sqlite";
 
 function ready(fn) {
   if (document.readyState != "loading") {
@@ -12,7 +12,7 @@ function ready(fn) {
 }
 
 function init() {
-  saveDB();
+  debugDB();
 
   const el = document.getElementById("root");
   ReactDOM.createRoot(el).render(<Root />);
