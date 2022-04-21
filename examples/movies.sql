3aa33182b0c3a843bfd65b3c915d0cf0b4ee860e
Yanglin Zhao <yz1876@gmail.com>
2022-04-20 21:51:59 -0400

    Add example data

--- examples/movies.sql
+++ examples/movies.sql
@@ -1,6 +1,20 @@
-CREATE TABLE COMPANY(
-   id INT PRIMARY KEY     NOT NULL,
-   name TEXT    NOT NULL,
-   ADDRESS        CHAR(50),
-   description CHAR(500)
+CREATE TABLE movies(
+  id INT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT
 );
+
+INSERT INTO
+  movies(id, name, description)
+VALUES
+  (1, 'Godfather', "Godfather");
+
+INSERT INTO
+  movies(id, name, description)
+VALUES
+  (2, 'Citizen Kane', "Citizen Kane");
+
+INSERT INTO
+  movies(id, name, description)
+VALUES
+  (3, 'Shawshank Redemption', "Shawshank Redemption");
--- examples/run.sh
+++ examples/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd examples
-rm movies.db && sqlite3 movies.db < movies.sql
+rm -f movies.db && sqlite3 movies.db < movies.sql
 cd -
